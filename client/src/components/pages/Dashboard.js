b68b436971d6084bf152b06938de51a7b5b64fcd
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-17 21:23:42 -0800

    updated calendar widget

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,41 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.min.css';
-import { Form, Button } from 'react-bulma-components';
-import { Container } from 'react-bootstrap';
+import { Form, Button, Block, Card } from 'react-bulma-components';
+import { Container, Row, Col } from 'react-bootstrap';
+import Calendar from '../widgets/Calendar';
 
 export default function Dashboard() {
   return (
-    <Container> <header className='header'>Search</header>
-      <Form.Field>
-        <Form.Control>
-          <Form.Input placeholder='Position title'/>
-        </Form.Control>
-      </Form.Field>
+    <Container> 
+      <Row>
+        <Col>
+          <Card.Content>
+            <Card.Header>
+              Search
+            </Card.Header>
+            <Form.Field>
+              <Form.Control>
+                <Form.Input placeholder='Position title'/>
+              </Form.Control>
+            </Form.Field>
 
-      <Form.Field>
-        <Form.Control>
-          <Form.Input placeholder='Zip Code'/>
-        </Form.Control>
-      </Form.Field>
+            <Form.Field>
+              <Form.Control>
+                <Form.Input placeholder='Zip Code'/>
+              </Form.Control>
+            </Form.Field>
 
-      <Form.Field>
-        <Form.Control>
-          <Button color="link">Search</Button>
-        </Form.Control>
-      </Form.Field>
+            <Form.Field>
+              <Form.Control>
+                <Button color="link">Search</Button>
+              </Form.Control>
+            </Form.Field>
 
-      <Form.Field>
-        <Form.Control>
-          <Form.Input placeholder='Company Name'/>
-        </Form.Control>
-      </Form.Field>
+            <Form.Field>
+              <Form.Control>
+                <Form.Input placeholder='Company Name'/>
+              </Form.Control>
+            </Form.Field>
 
-      <Form.Field>
-        <Form.Control>
-          <Button color="link">Search</Button>
-        </Form.Control>
-      </Form.Field>
+            <Form.Field>
+              <Form.Control>
+                <Button color="link">Search</Button>
+              </Form.Control>
+            </Form.Field>
+          </Card.Content>
+        </Col>
+
+        <Col>
+        
+        </Col>
+
+        <Col>
+          <Calendar/>
+        </Col>
+
+      </Row>
 
     </Container>
+
   )
 }
