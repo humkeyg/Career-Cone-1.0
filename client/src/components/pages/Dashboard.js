99afbdf4150388595cc1e2d9e997d4f0d7db7cf8
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-17 21:34:36 -0800

    updated dashboard.js

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -6,51 +6,16 @@ import Calendar from '../widgets/Calendar';
 
 export default function Dashboard() {
   return (
-    <Container> 
+    <Container>
+
       <Row>
-        <Col>
-          <Card.Content>
-            <Card.Header>
-              Search
-            </Card.Header>
-            <Form.Field>
-              <Form.Control>
-                <Form.Input placeholder='Position title'/>
-              </Form.Control>
-            </Form.Field>
-
-            <Form.Field>
-              <Form.Control>
-                <Form.Input placeholder='Zip Code'/>
-              </Form.Control>
-            </Form.Field>
-
-            <Form.Field>
-              <Form.Control>
-                <Button color="link">Search</Button>
-              </Form.Control>
-            </Form.Field>
-
-            <Form.Field>
-              <Form.Control>
-                <Form.Input placeholder='Company Name'/>
-              </Form.Control>
-            </Form.Field>
-
-            <Form.Field>
-              <Form.Control>
-                <Button color="link">Search</Button>
-              </Form.Control>
-            </Form.Field>
-          </Card.Content>
-        </Col>
-
-        <Col>
         
-        </Col>
-
         <Col>
           <Calendar/>
+        </Col>
+
+        <Col>
+          <Notes/>
         </Col>
 
       </Row>
