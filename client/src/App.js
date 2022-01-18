97b2c413b6d7aa39757fa69fb0bd0691bcf5924c
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-17 21:20:41 -0800

    removed calendar, notes, and progress from navigation.js and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Dashboard from './components/pages/Dashboard';
 import Jobs from './components/pages/Jobs';
-import Calendar from './components/pages/Calendar';
-import Progress from './components/pages/Progress';
 import Saved from './components/pages/Saved';
-import Notes from './components/pages/Notes';
 import About from './components/pages/About';
 import Footer from './components/Footer';
 
@@ -21,17 +18,8 @@ function App() {
     if (currentPage === 'Jobs') {
       return <Jobs/>;
     }
-    if (currentPage === 'Calendar') {
-      return <Calendar/>;
-    }
-    if (currentPage === 'Progress') {
-      return <Progress/>;
-    }
     if (currentPage === 'Saved') {
       return <Saved/>;
-    }
-    if (currentPage === 'Notes') {
-      return <Notes/>;
     }
     if (currentPage === 'About') {
       return <About/>;
