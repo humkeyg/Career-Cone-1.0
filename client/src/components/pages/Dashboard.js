037e0a319cfaf8d3716dd20fc1215986f3cf738a
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-19 20:20:01 -0800

    re-pushing because accidentally pulled from main

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -3,6 +3,7 @@ import 'bulma/css/bulma.min.css';
 import Notes from '../widgets/Notes.js';
 import { Form, Button, Block, Card } from 'react-bulma-components';
 import { Container, Row, Col } from 'react-bootstrap';
+import Notes from '../widgets/Notes';
 
 export default function Dashboard() {
   return (
