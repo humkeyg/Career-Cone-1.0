1972cb45606b8199ddf2e27873bdb09bb3f4ad17
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-19 20:51:01 -0800

    putting calendar back where it belongs

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import 'bulma/css/bulma.min.css';
 import Notes from '../widgets/Notes.js';
-import { Form, Button, Block, Card } from 'react-bulma-components';
 import { Container, Row, Col } from 'react-bootstrap';
+import Notes from '../widgets/Notes';
 
 export default function Dashboard() {
   return (
     <Container>
 
       <Row>
+
+        <Col>
+          <iframe className='calendarstyle' src="https://calendar.google.com/calendar/embed?src=i3cdofnqk0s122tmc9mbgnhj3c%40group.calendar.google.com&ctz=America%2FLos_Angeles"></iframe>
+        </Col>
         
         <Col>
           <Notes/>
