a611f5417ae1b77750d13ccc42d6a65f8806f6bf
Korey Cheshire <korey.cheshire@gmail.com>
2022-01-19 19:01:56 -0800

    removed calendar from main

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -3,6 +3,7 @@ import 'bulma/css/bulma.min.css';
 import { Form, Button, Block, Card } from 'react-bulma-components';
 import { Container, Row, Col } from 'react-bootstrap';
 import Calendar from '../widgets/Calendar';
+import Notes from '../widgets/Notes';
 
 export default function Dashboard() {
   return (
