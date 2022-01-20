bcc6f2d4a89614860a11266258120ac8c9749cde
DDiaz <daniel.diaz.0515@gmail.com>
2022-01-19 22:08:22 -0800

    server and client connected with appolo/mongoose

---
+++ client/src/components/pages/Saved.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bulma/css/bulma.min.css";
+import { Form, Button } from "react-bulma-components";
+import { Container } from "react-bootstrap";
+
+export default function Dashboard() {
+  return (
+    <Container>
+      {" "}
+      <header className="header">Search</header>
+    </Container>
+  );
+}
