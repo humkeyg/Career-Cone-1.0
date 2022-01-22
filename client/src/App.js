b335883702a8a948a71dc78e49f2fb3608ed8f4a
humkeyg <87458845+humkeyg@users.noreply.github.com>
2022-01-22 10:51:44 -0800

    navigation changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,87 @@
-import React, {useState} from 'react';
-import './App.css';
-import Header from './components/Header';
-import Dashboard from './components/pages/Dashboard';
-import Jobs from './components/pages/Jobs';
-import Saved from './components/pages/Saved';
-import About from './components/pages/About';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
+import "./App.css";
+import Header from "./components/Header";
+import Dashboard from "./components/pages/Dashboard";
+import Jobs from "./components/pages/Jobs";
+import Saved from "./components/pages/Saved";
+import About from "./components/pages/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navigation";
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// function App() {
+//   const [currentPage, setCurrentPage] = useState('Dashboard');
+
+//   const renderPage = () => {
+//     if (currentPage === 'Dashboard') {
+//       return <Dashboard/>;
+//     }
+//     if (currentPage === 'Jobs') {
+//       return <Jobs/>;
+//     }
+//     if (currentPage === 'Saved') {
+//       return <Saved/>;
+//     }
+//     if (currentPage === 'About') {
+//       return <About/>;
+//     }
+//   };
+
+//   return (
+//     <div className='App'>
+//       <Header currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+//       {renderPage()}
+//       <Footer/>
+//     </div>
+//   );
+// }
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Dashboard');
-
-  const renderPage = () => {
-    if (currentPage === 'Dashboard') {
-      return <Dashboard/>;
-    }
-    if (currentPage === 'Jobs') {
-      return <Jobs/>;
-    }
-    if (currentPage === 'Saved') {
-      return <Saved/>;
-    }
-    if (currentPage === 'About') {
-      return <About/>;
-    }
-  };
-
   return (
-    <div className='App'>
-      <Header currentPage={currentPage} setCurrentPage={setCurrentPage}/>
-      {renderPage()}
-      <Footer/>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/jobs" component={Jobs} />
+          <Route exact path="/saved" component={Saved} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+        <Footer />
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,26 +1,40 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
 import 'bulma/css/bulma.min.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import Notes from '../widgets/Notes';
 import '../widgets/Calendar.css'
+=======
+import React, { Component } from "react";
+import "bulma/css/bulma.min.css";
+import Notes from "../widgets/Notes.js";
+import { Form, Button, Block, Card } from "react-bulma-components";
+import { Container, Row, Col } from "react-bootstrap";
+import "../widgets/Calendar.css";
+>>>>>>> 7dba671a7b4a4a4805ee23245cde87550e1f0436
 
 export default function Dashboard() {
   return (
     <Container>
-
       <Row>
+<<<<<<< HEAD
 
         <Col>
           <iframe className='calendarstyle' src="https://calendar.google.com/calendar/embed?src=i3cdofnqk0s122tmc9mbgnhj3c%40group.calendar.google.com&ctz=America%2FLos_Angeles"></iframe>
         </Col>
         
+=======
+>>>>>>> 7dba671a7b4a4a4805ee23245cde87550e1f0436
         <Col>
-          <Notes/>
+          <iframe
+            className="calendarstyle"
+            src="https://calendar.google.com/calendar/embed?src=i3cdofnqk0s122tmc9mbgnhj3c%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+          ></iframe>
         </Col>
-
+        <Col>
+          <Notes />
+        </Col>
       </Row>
-
     </Container>
-
-  )
+  );
 }
