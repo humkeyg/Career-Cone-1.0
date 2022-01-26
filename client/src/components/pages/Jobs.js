414611b490e1f6edbf2270f49fa2e7398f80ada1
Alison Funkhouser <alison.m.funkhouser@gmail.com>
2022-01-25 17:48:05 -0800

    save changes

--- client/src/components/pages/Jobs.js
+++ client/src/components/pages/Jobs.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Form, Button } from "react-bulma-components";
+import { Form, Button, Box, Block, Notification} from "react-bulma-components";
 import { Container } from "react-bootstrap";
 import "./Jobs.css";
 
@@ -34,6 +34,50 @@ export default function Jobs() {
           <Button className="srchBtn" color="primary">Search</Button>
         </Form.Control>
       </Form.Field>
+
+      <Box>
+  <React.Fragment key=".0">
+    <Block>
+      <Notification color="info">
+    <Button className= "save" color="">
+      Save
+    </Button>
+        Some text
+      </Notification>
+    </Block>
+    <Block>
+      <Notification color="success">
+      <Button className= "save" color="">
+      Save
+    </Button>
+        Some more text
+      </Notification>
+    </Block>
+    <Block>
+      <Notification color="danger">
+      <Button className= "save" color="">
+      Save
+    </Button>
+        All of this are evently spaced
+      </Notification>
+    </Block>
+    <Block>
+      <Notification color="warning">
+      <Button className= "save" color="">
+      Save
+    </Button>
+        This one does not generate extra margin at the bottom
+      </Notification>
+    </Block>
+  </React.Fragment>
+</Box>
+
+<hr />
+
+
     </Container>
+
+
+
   );
 }
