63708398df8956f0c9a0b88e049e011d0d318c17
Alison Funkhouser <alison.m.funkhouser@gmail.com>
2022-01-25 19:19:45 -0800

    got api to consolelog

--- client/src/components/pages/Jobs.js
+++ client/src/components/pages/Jobs.js
@@ -1,27 +1,77 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Form, Button, Box, Block, Notification} from "react-bulma-components";
 import { Container } from "react-bootstrap";
 import "./Jobs.css";
 
 export default function Jobs() {
+  const [formState, setFormState] = useState({
+    jobs: "",
+    zipCode: ""
+  });
+  function searchJobs(jobs, zipCode) {
+    var jobLocation = "&LocationName=" + zipCode;
+    var jobCategory = "PositionTitle=" + jobs;
+    var host = "data.usajobs.gov";
+    var userAgent = "daniel.diaz.0515@gmail.com";
+    var authKey = "TM6TEbYm8310tpJD+9CyVa7cpguDIpbSZKZwSgnukTU=";
+    var url =
+      "https://data.usajobs.gov/api/search?" +
+      jobCategory +
+      jobLocation +
+      "&ResultsPerPage=5";
+
+
+      fetch(url, {
+        method: "GET",
+    
+        headers: {
+          Host: host,
+          "User-Agent": userAgent,
+          "Authorization-Key": authKey,
+        },
+      }).then(res => {
+        return res.json();
+      }).then(data => {
+        console.log(data)
+        // Map Over each response and append to section of the JSX
+      })
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    searchJobs(formState.jobs, formState.zipCode);
+    setFormState({
+      jobs: "",
+      zipCode: ""
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   return (
     <Container>
       {" "}
       <header className="header">Search</header>
+      <form onSubmit={handleFormSubmit}>
       <Form.Field>
         <Form.Control>
-          <Form.Input placeholder="Position title" />
+          <Form.Input name='jobs' placeholder="Position title" onChange={handleChange}/>
         </Form.Control>
       </Form.Field>
       <Form.Field>
         <Form.Control>
-          <Form.Input placeholder="Zip Code" />
+          <Form.Input name='zipCode' placeholder="Zip Code" onChange={handleChange}/>
         </Form.Control>
       </Form.Field>
       <Form.Field>
         <Form.Control>
-          <Button color="primary">Search</Button>
+          <Button id="searchBtn" color="primary" type="submit">Search</Button>
         </Form.Control>
       </Form.Field>
       <Form.Field>
@@ -34,6 +84,7 @@ export default function Jobs() {
           <Button className="srchBtn" color="primary">Search</Button>
         </Form.Control>
       </Form.Field>
+    </form>
 
       <Box>
   <React.Fragment key=".0">
@@ -63,7 +114,7 @@ export default function Jobs() {
     </Block>
     <Block>
       <Notification color="warning">
-      <Button className= "save" color="">
+      <Button className= "save" color="" >
       Save
     </Button>
         This one does not generate extra margin at the bottom
